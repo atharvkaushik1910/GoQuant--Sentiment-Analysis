6269b949f9604d48785637607d3a6c4ea04beee6
atharvkaushik1910 <84839739+atharvkaushik1910@users.noreply.github.com>
2025-07-16 17:43:08 +0530

    Update twitter_ingest.py

--- twitter_ingest.py
+++ twitter_ingest.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyNz/ILQW2X0H++XWvZvGK8Z",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
